0a4491db7c6caad0ee996f16732e3f7ad0b44686
Yuzheng Dun <yuzhengdun@Yuzhengs-MacBook-Pro.local>
2023-02-12 13:18:24 -0500

    completed assignment

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -4,4 +4,8 @@ for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
   # Fill in
+  sed -i "$linenum i The is the line $commit_count." foo.tex
+  pdflatex foo.tex
+  git add -u
+  git commit -m "Add $commit_count sentence to TeX file"
 done
